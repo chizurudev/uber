544c2901b25a3152e1abab7db42196551ef5f5e0
chizurudev <wisdomchizuru100@gmail.com>
2025-12-21 20:50:29 +0100

    changes made

--- index.php
+++ index.php
@@ -21,63 +21,8 @@
 <?php require("mainheader.php")  ?>
 <br>
 <br>
+
 <body>
-
-     <!-- Navigation Bar -->
- <nav class="navbar navbar-expand-lg navbar-dark custom-navbar">
-   <div class="container-fluid">
-     <!-- Brand -->
-     <a class="navbar-brand" href="#">Uber</a>
-
-     <!-- Mobile Toggle Button -->
-     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent">
-       <span class="navbar-toggler-icon"></span>
-     </button>
-
-     <!-- Navigation Content -->
-     <div class="collapse navbar-collapse" id="navbarContent">
-       <!-- Left Side: 3 Links + 1 Dropdown -->
-       <ul class="navbar-nav me-auto">
-         <li class="nav-item">
-           <a class="nav-link active" href="#">Ride</a>
-         </li>
-         <li class="nav-item">
-           <a class="nav-link" href="#">Drive</a>
-         </li>
-         <li class="nav-item">
-           <a class="nav-link" href="#">Uber Eats</a>
-         </li>
-         <li class="nav-item dropdown">
-           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
-             About
-           </a>
-           <ul class="dropdown-menu">
-             <li><a class="dropdown-item" href="#">Product 1</a></li>
-             <li><a class="dropdown-item" href="#">Product 2</a></li>
-             <li><a class="dropdown-item" href="#">Product 3</a></li>
-             <li>
-               <hr class="dropdown-divider">
-             </li>
-             <li><a class="dropdown-item" href="#">All Products</a></li>
-           </ul>
-         </li>
-       </ul>
-
-       <!-- Right Side: 2 Links + Sign Up Button -->
-       <ul class="navbar-nav">
-         <li class="nav-item">
-           <a class="nav-link" href="#">Contact</a>
-         </li>
-         <li class="nav-item">
-           <a class="nav-link" href="#">Login</a>
-         </li>
-         <li class="nav-item">
-           <a class="btn btn-signup ms-2" href="#">Sign Up</a>
-         </li>
-       </ul>
-     </div>
-   </div>
- </nav>
     <div class="container">
         <div class="row">
             <div class="col-sm-12 col-lg-5">
@@ -210,7 +155,7 @@
                 <p>Blog</p><br>
                 <p>Careers</p><br>
             </div>
-            
+
             <div class="col-sm-12 col-lg-3">
                 <h5>Products</h5><br>
                 <p>Ride</p><br>
@@ -219,13 +164,13 @@
                 <p>Uber Freight</p><br>
                 <p>Gift cards</p><br>
             </div>
-          
+
             <div class="col-sm-12 col-lg-3">
                 <h5>Global Citizenship</h5><br>
                 <p>Safety</p><br>
                 <p>Sustainability</p><br>
             </div>
-        
+
             <div class="col-sm-12 col-lg-3">
                 <h5>Travel</h5><br>
                 <p>Reserve</p><br>
@@ -246,8 +191,26 @@
             </div>
 
             <div class="left-side">
-                <button class="btn"> <i class="fa-solid fa-globe"></i> <p> English </p>  </button>
-                <button class="btn"> <i class="fa-solid fa-location-crosshairs"></i> <p> San Francisco Buy Area </p>  </button>
+                <button class="btn"> <i class="fa-solid fa-globe"></i>
+                    <p> English </p>
+                </button>
+                <button class="btn"> <i class="fa-solid fa-location-crosshairs"></i>
+                    <p> San Francisco Buy Area </p>
+                </button>
+            </div>
+        </div>
+        <br>
+        <br>
+
+        <div class="down-footer">
+            <div>
+                &copy; 2025 Uber technologies
+            </div>
+
+            <div class="left-side">
+                <p><a href=""></a>Privacy</p>
+                <p><a href=""></a>Accesibility</p>
+                <p><a href=""></a>Terms</p>
             </div>
         </div>
     </div>
@@ -264,8 +227,8 @@
 
 </html>
 <!-- <?php
-$a = 1;
-if ($a == 2) {
-    echo "a=2";
-}
-?> -->
+        $a = 1;
+        if ($a == 2) {
+            echo "a=2";
+        }
+        ?> -->
