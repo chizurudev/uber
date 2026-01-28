cab5a3155dd402866616891a5adb2972f433aeff
chizurudev <wisdomchizuru100@gmail.com>
2026-01-29 00:52:24 +0100

    changes made

--- js/signup.js
+++ js/signup.js
@@ -1,8 +1,9 @@
 let blnState = 0;
 let btnShow = document.getElementById("show")
+
 const showPass = () => {
     let password = document.getElementById("txtPassword");
-    if (blnState === 0) {
+    if (blnState == 0) {
         password.type = "text";
         blnState = 1;
         btnShow.innerText = "Hide"
